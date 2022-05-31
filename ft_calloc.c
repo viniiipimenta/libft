355ddf3b130dc8aa8f87a2c1bdc07e71c6d3f246
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-05-31 07:16:17 -0700

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 14:37:34 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/05/27 15:14:18 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/05/31 11:13:35 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	int		*locale;
-	
+
 	locale = malloc(count * size);
 	if (locale == NULL)
 		return (0);
