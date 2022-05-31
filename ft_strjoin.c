cff60611f235c458bfc6b64164638c77add240ad
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-05-31 07:11:32 -0700

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 11:32:58 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/05/31 10:03:23 by mpimenta         ###   ########.fr       */
+/*   Created: 2022/05/30 13:57:24 by mpimenta          #+#    #+#             */
+/*   Updated: 2022/05/31 11:07:18 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*s1;
-	size_t	i;
+	char	*s3;
+	size_t	len_s1;
+	size_t	len_s2;
 
-	i = 0;
-	if (!s)
+	if (!s1 || !s2)
 		return (0);
-	if (start > ft_strlen(s))
-		return (ft_strdup("\0"));
-	s1 = malloc(sizeof(char) * (len + 1));
-	if (!s1)
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	s3 = malloc(sizeof(char) * (len_s1 + len_s2));
+	if (!s3)
 		return (0);
-	while (i < len && s[start + i] != '\0')
-	{
-		s1[i] = s[start + i];
-		i++;
-	}
-	s1[i] = '\0';
-	return (s1);
+	ft_strlcpy(s3, s1, ft_strlen(s1) + 1);
+	ft_strlcat(s3, s2, ft_strlen(s3) + ft_strlen(s2) + 1);
+	return (s3);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:39:39 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/05/29 16:36:11 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/05/30 14:07:16 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,5 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
