632cc4e1d4fd314132f3e679d73b918469336afd
leeticiamm <106685430+leeticiamm@users.noreply.github.com>
2022-06-01 14:24:55 -0300

    Add files via upload

---
+++ ft_putnbr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmagalha <lmagalha@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 23:22:42 by lmagalha          #+#    #+#             */
+/*   Updated: 2021/12/17 23:29:44 by lmagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	nbr;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nbr = nb * -1;
+	}
+	else
+	{
+		nbr = nb;
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	ft_putchar(nbr % 10 + '0');
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr(42);
+// 	return (0);
+// }
